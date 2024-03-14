4db615afd40337de0d260697ac55869b5ea2f19d
Enoch Daniel <120383747+EnochDaniel123@users.noreply.github.com>
2024-03-14 13:34:05 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,11 @@
+Class A{
+    int a ,b,c;
+    void add()
+    {a = 10 ; b = 20 ;
+    c = a+b;
+    System.out.println("Sum of two number "+c);}
+    void sub()
+    {a = 200 ; b = 100;
+    c = a-b;
+    System.out.println("Sum of two number "+c);}
+}
